57ff745ba275cfa54921870f4e07a01bcaab32c1
nikhilkrp <150312021+nikhilkrp@users.noreply.github.com>
2025-10-30 19:11:55 +0530

    import error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import { Routes,Route } from "react-router-dom"
 import Dashboard from './pages/Dashboard'
 import SignIn from "./pages/SignIn"
-import Profile from "./pages/profile"
+import Profile from "./pages/Profile"
 import ProtectedRoute from "./components/ProtectedRoute"
 
 
