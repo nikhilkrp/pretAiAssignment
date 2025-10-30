cc9dea7c0c9da789668b5c3a96cee517bbedf7c4
nikhilkrp <150312021+nikhilkrp@users.noreply.github.com>
2025-10-30 19:28:13 +0530

    connected front end to backend

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:process.env.FRONTEND,
     credentials: true,
   })
 );
